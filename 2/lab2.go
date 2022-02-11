a9bb8a1b36212951b31ae805717a58efdf6ac314
Oleksii Dobrovolskyi <bardianofmandalore@gmail.com>
2022-02-11 20:58:07 +0200

    [lab2] implemented stastics, minor improvements

--- 2/lab2.go
+++ 2/lab2.go
@@ -9,12 +9,52 @@ import (
 	"time"
 )
 
+var (
+	formatLog func(string, ...interface{}) = func(format string, a ...interface{}) {
+		fmt.Printf(format, a...)
+	}
+)
+
+type Statistics struct {
+	TotalProcesses     int
+	FinishedProcesses  int
+	LostProcesses      int
+	DestroyedProcesses int
+	MaxQueueLength     int
+
+	curQueueLength int
+	mutex          sync.Mutex
+}
+
+func (s *Statistics) ModifyStatistics(f func(*Statistics)) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	f(s)
+}
+
+func (s *Statistics) ChangeQueueLength(amount int) {
+	s.ModifyStatistics(func(s *Statistics) {
+		s.curQueueLength += amount
+		if s.curQueueLength > s.MaxQueueLength {
+			s.MaxQueueLength = s.curQueueLength
+		}
+	})
+}
+
+func (s *Statistics) Print() {
+	fmt.Println("Processes:")
+	fmt.Printf("  Total     %d\n", s.TotalProcesses)
+	fmt.Printf("  Finished  %d\t%f%%\n", s.FinishedProcesses, float32(s.FinishedProcesses)/float32(s.TotalProcesses)*100)
+	fmt.Printf("  Lost      %d\t%f%%\n", s.LostProcesses, float32(s.LostProcesses)/float32(s.TotalProcesses)*100)
+	fmt.Printf("  Destroyed %d\t%f%%\n", s.DestroyedProcesses, float32(s.DestroyedProcesses)/float32(s.TotalProcesses)*100)
+	fmt.Println("Max queue length:", s.MaxQueueLength)
+}
+
 type Process struct {
 	ParentId int
 	Id       int
 }
 
-// I need to figure out how to send the DONE signal from here
 type ProcessGenerator struct {
 	Id                  int
 	ProcessesToGenerate int
@@ -39,7 +79,7 @@ func (p *ProcessGenerator) Run() {
 			ParentId: p.Id,
 			Id:       i,
 		}
-		fmt.Printf("GEN%d: ==> %d_%d\n", process.ParentId, process.ParentId, process.Id)
+		formatLog("GEN%d: ==> %d_%d\n", process.ParentId, process.ParentId, process.Id)
 
 		p.SchedulerQueue <- process
 	}
@@ -52,6 +92,8 @@ type Cpu struct {
 	GeneralQueue chan Process
 	DirectQueue  chan Process
 	Wg           *sync.WaitGroup
+
+	*Statistics
 
 	IsBusy          bool
 	CurProcess      Process
@@ -71,13 +113,13 @@ func (c *Cpu) Run() {
 		c.CurProcess = p
 		c.CurProcessMutex.Unlock()
 
-		fmt.Printf("CPU%d: <== %d_%d\n", c.Id, p.ParentId, p.Id)
+		formatLog("CPU%d: <== %d_%d\n", c.Id, p.ParentId, p.Id)
 
 		// simulate activity
 		timeToWait := (rand.Int() % (c.MaxProcessingTime - c.MinProcessingTime)) + c.MinProcessingTime
 		time.Sleep(time.Millisecond * time.Duration(timeToWait))
 
-		fmt.Printf("CPU%d: finished %d_%d\n", c.Id, p.ParentId, p.Id)
+		formatLog("CPU%d: finished %d_%d\n", c.Id, p.ParentId, p.Id)
 
 		c.CurProcessMutex.Lock()
 		c.IsBusy = false
@@ -93,6 +135,7 @@ func (c *Cpu) Run() {
 				return
 			}
 			process = p
+			c.ChangeQueueLength(-1)
 		case process = <-c.DirectQueue:
 		}
 
@@ -114,6 +157,8 @@ type Scheduler struct {
 	Cpu1 *Cpu
 	Cpu2 *Cpu
 
+	*Statistics
+
 	SchedulerQueue chan Process
 	CpuQueue       chan Process
 }
@@ -126,7 +171,7 @@ func (s *Scheduler) Run() {
 	}()
 
 	pushToDirectQueue := func(c *Cpu, p Process) {
-		fmt.Printf("SCHD: %d_%d ==> Cpu%d\n", p.ParentId, p.Id, c.Id)
+		formatLog("SCHD: %d_%d ==> Cpu%d\n", p.ParentId, p.Id, c.Id)
 		c.DirectQueue <- p
 	}
 
@@ -139,7 +184,8 @@ func (s *Scheduler) Run() {
 				return
 			}
 			if cpu1Process.ParentId != 1 {
-				fmt.Printf("SCHD: %d_%d is lost\n", p.ParentId, p.Id)
+				formatLog("SCHD: %d_%d is lost\n", p.ParentId, p.Id)
+				s.ModifyStatistics(func(s *Statistics) { s.LostProcesses++ })
 				return
 			}
 			_, cpu2Busy := s.Cpu2.GetCurrentProcess()
@@ -147,17 +193,19 @@ func (s *Scheduler) Run() {
 				pushToDirectQueue(s.Cpu2, p)
 				return
 			}
-			fmt.Printf("SCHD: %d_%d is destroyed\n", p.ParentId, p.Id)
+			formatLog("SCHD: %d_%d is destroyed\n", p.ParentId, p.Id)
+			s.ModifyStatistics(func(s *Statistics) { s.DestroyedProcesses++ })
 		case 2:
 			_, cpu2Busy := s.Cpu2.GetCurrentProcess()
 			if cpu2Busy {
 				s.CpuQueue <- p
-				fmt.Printf("SCHD: %d_%d ==> CpuQueue\n", p.ParentId, p.Id)
+				s.ChangeQueueLength(1)
+				formatLog("SCHD: %d_%d ==> CpuQueue\n", p.ParentId, p.Id)
 			} else {
 				pushToDirectQueue(s.Cpu2, p)
 			}
 		default:
-			fmt.Println("Process has invalid ParentId =", p.ParentId)
+			formatLog("Process has invalid ParentId = %d\n", p.ParentId)
 		}
 	}
 
@@ -173,73 +221,21 @@ func (s *Scheduler) Run() {
 }
 
 func main() {
+	g1p := flag.Int("g1p", 15, "number of processes for GEN1 to generate")
+	g1m := flag.Int("g1m", 50, "min GEN1 process generation time")
+	g1M := flag.Int("g1M", 300, "max GEN1 process generation time")
 
-	schedulerQueue := make(chan Process)
-	cpuQueue := make(chan Process)
-	var genWg sync.WaitGroup
-	genWg.Add(2)
-	var cpuWg sync.WaitGroup
-	cpuWg.Add(2)
+	g2p := flag.Int("g2p", 15, "number of processes for GEN1 to generate")
+	g2m := flag.Int("g2m", 50, "min GEN1 process generation time")
+	g2M := flag.Int("g2M", 300, "max GEN1 process generation time")
 
-	gen1 := ProcessGenerator{
-		Id:             1,
-		SchedulerQueue: schedulerQueue,
-		Wg:             &genWg,
-	}
+	c1m := flag.Int("c1m", 60, "min CPU1 processing time")
+	c1M := flag.Int("c1M", 200, "max CPU1 processing time")
 
-	gen2 := ProcessGenerator{
-		Id:             2,
-		SchedulerQueue: schedulerQueue,
-		Wg:             &genWg,
-	}
+	c2m := flag.Int("c2m", 30, "min CPU1 processing time")
+	c2M := flag.Int("c2M", 100, "max CPU1 processing time")
 
-	cpu1 := Cpu{
-		Id:           1,
-		GeneralQueue: cpuQueue,
-		DirectQueue:  make(chan Process),
-		Wg:           &cpuWg,
-
-		IsBusy:          false,
-		CurProcess:      Process{},
-		CurProcessMutex: sync.Mutex{},
-	}
-
-	cpu2 := Cpu{
-		Id:           2,
-		GeneralQueue: cpuQueue,
-		DirectQueue:  make(chan Process),
-		Wg:           &cpuWg,
-
-		IsBusy:          false,
-		CurProcess:      Process{},
-		CurProcessMutex: sync.Mutex{},
-	}
-
-	scheduler := Scheduler{
-		Gen1: &gen1,
-		Gen2: &gen2,
-		Cpu1: &cpu1,
-		Cpu2: &cpu2,
-
-		SchedulerQueue: schedulerQueue,
-		CpuQueue:       cpuQueue,
-		GenWg:          &genWg,
-	}
-
-	flag.IntVar(&gen1.ProcessesToGenerate, "g1p", 15, "number of processes for GEN1 to generate")
-	flag.IntVar(&gen1.MinProcessGenerationTime, "g1m", 50, "min GEN1 process generation time")
-	flag.IntVar(&gen1.MaxProcessGenerationTime, "g1M", 300, "max GEN1 process generation time")
-
-	flag.IntVar(&gen2.ProcessesToGenerate, "g2p", 15, "number of processes for GEN1 to generate")
-	flag.IntVar(&gen2.MinProcessGenerationTime, "g2m", 50, "min GEN1 process generation time")
-	flag.IntVar(&gen2.MaxProcessGenerationTime, "g2M", 300, "max GEN1 process generation time")
-
-	flag.IntVar(&cpu1.MinProcessingTime, "c1m", 60, "min CPU1 processing time")
-	flag.IntVar(&cpu1.MaxProcessingTime, "c1M", 200, "max CPU1 processing time")
-
-	flag.IntVar(&cpu2.MinProcessingTime, "c2m", 30, "min CPU1 processing time")
-	flag.IntVar(&cpu2.MaxProcessingTime, "c2M", 100, "max CPU1 processing time")
-
+	logOn := flag.Bool("log", false, "whether to log runtime info")
 	printHelp := flag.Bool("help", false, "print this message")
 
 	flag.Parse()
@@ -249,11 +245,89 @@ func main() {
 		os.Exit(0)
 	}
 
+	if !*logOn {
+		formatLog = func(s string, i ...interface{}) {}
+	}
+
+	stat := Statistics{
+		TotalProcesses: *g1p + *g2p,
+	}
+
+	schedulerQueue := make(chan Process)
+	cpuQueue := make(chan Process, *g1p+*g2p)
+	var genWg sync.WaitGroup
+	genWg.Add(2)
+	var cpuWg sync.WaitGroup
+	cpuWg.Add(2)
+
+	gen1 := ProcessGenerator{
+		Id:             1,
+		SchedulerQueue: schedulerQueue,
+		Wg:             &genWg,
+
+		ProcessesToGenerate:      *g1p,
+		MinProcessGenerationTime: *g1m,
+		MaxProcessGenerationTime: *g1M,
+	}
+
+	gen2 := ProcessGenerator{
+		Id:             2,
+		SchedulerQueue: schedulerQueue,
+		Wg:             &genWg,
+
+		ProcessesToGenerate:      *g2p,
+		MinProcessGenerationTime: *g2m,
+		MaxProcessGenerationTime: *g2M,
+	}
+
+	cpu1 := Cpu{
+		Id:           1,
+		GeneralQueue: cpuQueue,
+		DirectQueue:  make(chan Process),
+		Wg:           &cpuWg,
+
+		Statistics: &stat,
+
+		MinProcessingTime: *c1m,
+		MaxProcessingTime: *c1M,
+	}
+
+	cpu2 := Cpu{
+		Id:           2,
+		GeneralQueue: cpuQueue,
+		DirectQueue:  make(chan Process),
+		Wg:           &cpuWg,
+
+		Statistics: &stat,
+
+		MinProcessingTime: *c2m,
+		MaxProcessingTime: *c2M,
+	}
+
+	scheduler := Scheduler{
+		Gen1: &gen1,
+		Gen2: &gen2,
+		Cpu1: &cpu1,
+		Cpu2: &cpu2,
+
+		Statistics: &stat,
+
+		SchedulerQueue: schedulerQueue,
+		CpuQueue:       cpuQueue,
+		GenWg:          &genWg,
+	}
+
 	go cpu1.Run()
 	go cpu2.Run()
 	go gen1.Run()
 	go gen2.Run()
 
+	fmt.Println("Running...")
+
 	scheduler.Run()
 	cpuWg.Wait()
+
+	stat.FinishedProcesses = stat.TotalProcesses - stat.LostProcesses - stat.DestroyedProcesses
+	fmt.Println()
+	stat.Print()
 }
